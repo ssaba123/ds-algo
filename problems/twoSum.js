93dabe56a0b55949635e56c5e2bd1b1ccd3c9307
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-27 10:56:34 -0400

    Test multiple examples

--- problems/twoSum.js
+++ problems/twoSum.js
@@ -1,20 +1,29 @@
 var twoSum = function(nums, target) {
-// start at the first number
+// start pointer at first position (index of 0)
     let curr = 0;
+// set num1 to element at curr position
     let num1 = nums[curr];
+// set an empty index
     let index = '';
-    
+
+    // create a loop to go through every element in the array
     for(let i = 1; i <= nums.length -1; i++ ){
         // if the two numbers = target, return the index 
         if(num1 + nums[i] === target){
            index = `[${curr}, ${i}]`
-        } else { // otherwise make the first number, the number next to the current pointer
+        } else { 
+            // otherwise move the pointer up one position
             curr++;
+            // set pointer to new element
             num1 = nums[curr]
         }
-    }   
+    }  
+    
+    // return the index
     return index
 };
 
 
-console.log(twoSum([3,3], 6))
+console.log(twoSum([2,7,11,15], 9)) // expected output: [0,1]
+console.log(twoSum([22,9,1,2], 10)) // expected output: [1,2]
+console.log(twoSum([10,7,30,190], 200)) // expected output: [0,3] CURRENTLY FAILS
