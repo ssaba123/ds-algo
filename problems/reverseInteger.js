54b339f13db173b92318afa7c1bc63374ae88e0c
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-27 10:42:20 -0400

    Add two sum and optimize reverseInteger

--- problems/reverseInteger.js
+++ problems/reverseInteger.js
@@ -1,25 +1,13 @@
-// Given an integer, return it's reversed form
+// Given an integer, return its reversed form
 
 // create the reverseInt function
 function reverseInt(int){
     // create a variable to store reversed integer
-    let reversed = '';
-    // turn number into a string
-    let num = int.toString()
-    // create a loop to go through each digit
-    for( let i = 0; i <= num.length -1; i++){
-        // add digit to beginning of reversed variable
-        reversed = num[i] + reversed
-    }
+    // to reverse, turn to string, then array, reverse and then back to string
+    let reversed = int.toString().split('').reverse().join('');
 
-    // if negative, move negative sign from end to front
-    if(reversed[reversed.length -1] === '-'){
-    reversed = '-' + reversed 
-    reversed = reversed.slice(0,-1)
-    }
-
-   // turn the string back to a number and return
-   return reversed
+   // turn the string back to a number (use Math.sign() to make it negative if int is negative) and return
+   return parseInt(reversed) * Math.sign(int)
 }
 
-console.log(reverseInt(-500)) // expected output: -005
+console.log(reverseInt(-879543)) // expected output: -345978
