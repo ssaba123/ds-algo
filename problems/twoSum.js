d552e00ba9b77cf3a81bc71f1fc5226d7af1d713
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-10-27 10:49:02 -0400

    Add current pointer

--- problems/twoSum.js
+++ problems/twoSum.js
@@ -1,19 +1,20 @@
 var twoSum = function(nums, target) {
 // start at the first number
-    let num1 = nums[0];
-
+    let curr = 0;
+    let num1 = nums[curr];
     let index = '';
     
     for(let i = 1; i <= nums.length -1; i++ ){
         // if the two numbers = target, return the index 
         if(num1 + nums[i] === target){
-           index = `[${nums[num1]}, ${i}]`
+           index = `[${curr}, ${i}]`
         } else { // otherwise make the first number, the number next to the current pointer
-            num1 = nums[0+i]
+            curr++;
+            num1 = nums[curr]
         }
     }   
     return index
 };
 
 
-console.log(twoSum([2,4,8,1,15], 9))
+console.log(twoSum([3,3], 6))
