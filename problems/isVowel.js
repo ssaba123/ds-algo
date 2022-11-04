18f39bdc7de491f817e0fa1c6e029b30ee239e64
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-04 17:38:09 -0400

    Refactor to remove variable

--- problems/isVowel.js
+++ problems/isVowel.js
@@ -1,6 +1,5 @@
 function vowelOne(word){
-   let newWord = word.replace(/[^aeiou]/ig, 0)   
-   return newWord.replace(/[aeiou]/ig, 1)  
+   return word.replace(/[^aeiou]/ig, 0).replace(/[aeiou]/ig, 1)  
    }
 
 console.log(vowelOne('hello'))
