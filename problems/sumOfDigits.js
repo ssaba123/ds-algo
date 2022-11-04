9a7240df1f6b18f47f177bca23ee2a6964fce328
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-04 18:16:42 -0400

    Get digital root

--- problems/sumOfDigits.js
+++ problems/sumOfDigits.js
@@ -1,5 +1,9 @@
-let phrase = 'This is the new sentence repeat repeat that that clue'
-let reg = /[aeiou]/ig
-let result = phrase.match(reg).map(i=> i=0)
+function digitalRoot(number) {
+   numArr = number.toString().split("")
+   number = numArr.reduce((a,b) => parseInt(a) + parseInt(b))
+    return number == 0 ? 0 : number > 9 ? digitalRoot(number) : number  
+}
 
-console.log(result)
+
+console.log(digitalRoot(128))
+
