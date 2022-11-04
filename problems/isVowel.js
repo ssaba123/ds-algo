46b318d9f503641332f404530c6966cd085af6cb
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-04 17:35:52 -0400

    Remove y from vowels

--- problems/isVowel.js
+++ problems/isVowel.js
@@ -1,6 +1,6 @@
 function vowelOne(word){
-   let newWord = word.replace(/[^aeiouy]/ig, 0)   
-   return newWord.replace(/[aeiouy]/ig, 1)  
+   let newWord = word.replace(/[^aeiou]/ig, 0)   
+   return newWord.replace(/[aeiou]/ig, 1)  
    }
 
 console.log(vowelOne('hello'))
