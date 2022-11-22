c760b6fc43721f5f7ccf1cb81551d71fb0123ea7
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-22 09:50:15 -0500

    return phone number from array

--- problems/phoneNum.js
+++ problems/phoneNum.js
@@ -1,5 +1,7 @@
 function createPhoneNumber(numbers){
-  return  numbers.join('').replace(/(\d{3})(\d{3})(\d{4})/,'($1) $2-$3');
-  }
+
+return numbers.reduce((curr,acc) => curr.replace('0', acc), "(000) 000-0000")
+
+}
 
   console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
