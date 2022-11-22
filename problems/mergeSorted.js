fdef59ec09680f5c3ef64bd828f48aab826c4335
sabashaikh1 <ss.sabashaikh1@gmail.com>
2022-11-22 09:51:32 -0500

    Merge two sorted arrays into one sorted array

--- problems/mergeSorted.js
+++ problems/mergeSorted.js
@@ -1,5 +1,18 @@
 function mergeSortedArrays(arr1, arr2){
    return [...arr1, ...arr2].sort((a,b)=> a - b)
+
+// const mergedArrays = [...arr1,...arr2]
+// const sortedArray = []
+// let point1 = 0;
+// let point2 = 0;
+// for(let i =0; i< mergedArrays.length; i++){
+//   if(mergedArrays[start])
+//    start++
+
+// }
+// return mergedArrays
+
+
 }
 
 console.log(mergeSortedArrays([2,4,6,14], [4,7,9,11,14]))
